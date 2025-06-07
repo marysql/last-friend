b6c46f397908e1f3a894cdce7dcc22467d703a70
MarySql <mary.mitsuri250@gmail.com>
2025-06-07 00:44:48 -0300

    feat: Criação da entidade

--- src/main/java/com/marysql/last_friend/model/Message.java
+++ src/main/java/com/marysql/last_friend/model/Message.java
@@ -1,4 +1,19 @@
 package com.marysql.last_friend.model;
 
 public class Message {
+    private String texto;
+
+    public Mensagem() {}
+
+    public Mensagem(String texto) {
+        this.texto = texto;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 }
